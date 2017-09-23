b6e070741b6e5d6147b35d9f13355812c09b2261
Rodrigo Mendoza <rodrigo.mendoza@usach.cl>
2017-09-23 02:19:45 -0300

    8vo commit

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 var express = require('express'),
 app = express();
+var options = {
+    index: "home.html"
+  };
+  
+  app.use('/', express.static('src/home', options));  
 //app.use(express.static('www'));
-app.use(express.static(__dirname + "/src"));
+//app.use(express.static(__dirname + "/src"));
 //app.use(express.static('src/pages/home/home.html'));
 app.set('port', process.env.PORT || 5000);
 app.listen(app.get('port'), function () {
